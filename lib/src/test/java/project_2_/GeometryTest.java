f3059026ba52c333da4a377f8f65af08e308d595
Milenka <Milenka@LAPTOP-FQGQS59A>
2021-08-11 20:44:14 +0100

    A TestNG test file called “GeometryTest.java” and a java file “Geometry.java”. in the TestNG test add the following four stub methods •	testAreaRect() •	testPerimeterRect() •	testVolumeSphere() •	testSurfaceAreaSphere()

---
+++ lib/src/test/java/project_2_/GeometryTest.java
@@ -0,0 +1,30 @@
+package project_2_;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class GeometryTest {
+
+  @Test
+  public void someLibraryMethodTest() {
+    throw new RuntimeException("Test not implemented")
+  }
+  
+  @Test void someLibraryMethodReturnsTrue() {
+      Geometry classUnderTest = new Geometry();
+      assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+  }
+  
+  @Test public void validateRectangleArea() {
+
+      float epsilon = 0.1f;
+      float height = 12.4f;
+      float width = 4.47f;
+      Geometry geometry = new Geometry();
+      assertEquals(geometry.areaRect(height, width), height * width,epsilon);
+ 
+}
+  
+}
